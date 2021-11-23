74c434181ebb561abb3772257b4a56f5100ba1e4
matikurcze00 <m.roszkowski00@gmail.com>
2021-11-23 11:28:49 +0100

    projekt done

--- LAB34/matlab-code/projekt/pro2_DMC_test.m
+++ LAB34/matlab-code/projekt/pro2_DMC_test.m
@@ -5,9 +5,9 @@ imax = 250;
 
 %Parametry dobrane eksperymentalnie
 D=120; %D z poprzedniego podpunktu
-N=90; 
-Nu=2; 
-lambda=0.6; 
+N=110; 
+Nu=2; %szczegolnie wazna jest mala liczba Nu
+lambda=0.3; %po zmniejszeniu Lambdy lepiej dziala na szum 
 Dz=21; %zoptymalizowane
 %dUmax = 1;
 
@@ -27,7 +27,9 @@ I = eye(Nu);
 U(1:11) = Upp;
 Yzad(1:9)=0;
 Yzad(10:imax)=1;
-Z(10:imax)=  Zskok + 0.1*sin(20*linspace(0,1,imax-9));
+Z(10:imax)=  Zskok;
+Zszum = zeros(imax,1);
+Zszum(10:imax) =  Zskok+ 0.1*sin(20*linspace(0,1,imax-9));
 
 
 u = U - Upp;
@@ -52,7 +54,7 @@ end
 K = (M'*M + lambda*I)^(-1)*M';
 
 for k = 10:imax
-    Y(k)=symulacja_obiektu2y_p2(U(k-5),U(k-6),Z(k-3),Z(k-4),Y(k-1),Y(k-2));
+    Y(k)=symulacja_obiektu2y_p2(U(k-5),U(k-6),Zszum(k-3),Zszum(k-4),Y(k-1),Y(k-2));
     y(k) = Y(k) - Ypp;
     e(k) = Yzad(k) - Y(k);
    
@@ -103,7 +105,7 @@ end
 
 E = (norm(e))^2
 
-f5 = figure;
+f9 = figure;
 subplot(3,1,1);
 stairs(U);
 title('u(k)');
@@ -121,3 +123,11 @@ hold on;
 stairs(Yzad);
 xlabel('k');
 legend('y','y_z_a_d','Location','southeast');
+
+f10 = figure;
+stairs(Zszum);
+title('Pomiar zak³óceñ z szumem');
+xlabel('k');
+ylabel('Z');
+
+
